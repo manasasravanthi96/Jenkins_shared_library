5e6b8ad7687de2a6c79ccfd081a2e4ba33b970ea
Prabhu-Manasa <36622595+manasasravanthi96@users.noreply.github.com>
2022-04-01 17:08:08 +0530

    Update sshConnection.groovy

--- vars/sshConnection.groovy
+++ vars/sshConnection.groovy
@@ -1,7 +1,7 @@
 def call(def JarFile_name,def host,def destinationPath)
 {
 	try {
-		sh 'scp JarFile_name host:destinationPath/JarFile_name'
+		sh 'scp ${JarFile_name} ${host}:${destinationPath}/${JarFile_name}'
 	}
 	catch(Exception th) {
 		echo "issue while copying file"
