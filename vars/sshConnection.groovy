cee0dd32ab557611fdbe002581a606021d19f579
Prabhu-Manasa <36622595+manasasravanthi96@users.noreply.github.com>
2022-04-01 17:15:31 +0530

    Update sshConnection.groovy

--- vars/sshConnection.groovy
+++ vars/sshConnection.groovy
@@ -1,7 +1,12 @@
-def call(def JarFile_name,def host,def destinationPath)
+def call(def server)
 {
 	try {
-		sh 'scp ${JarFile_name} ${host}:${destinationPath}/${JarFile_name}'
+		sh 'scp target/*.jar springboot@${server}:/var/springboot/simple-camel-logger-1.0.jar'
+		sh """ssh -tt springboot@${server} << EOF 
+                                sudo systemctl restart simple-camel-logger.service
+                                sudo systemctl status simple-camel-logger.service
+                                exit
+                                EOF"""
 	}
 	catch(Exception th) {
 		echo "issue while copying file"
