3f5eb87f47814e4fe5f4c48757860ef108bf020e
Prabhu-Manasa <36622595+manasasravanthi96@users.noreply.github.com>
2022-04-01 17:00:06 +0530

    Add files via upload

---
+++ vars/sshConnection.groovy
@@ -0,0 +1,10 @@
+def call(def JarFile_name,def host,def destinationPath)
+{
+	try {
+		sh 'scp JarFile_name host:destinationPath/JarFile_name'
+	}
+	catch(Exception th) {
+		echo "issue while copying file"
+		throw th;
+	}		
+}
