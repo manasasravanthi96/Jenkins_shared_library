f58846ed816b831ebf9240af12d1b62ea3682922
Prabhu-Manasa <36622595+manasasravanthi96@users.noreply.github.com>
2022-04-01 19:48:25 +0530

    Update sshConnection.groovy

--- vars/sshConnection.groovy
+++ vars/sshConnection.groovy
@@ -1,9 +1,13 @@
 def call(def server)
 {
 	try {
+		sh """ ssh -tt springboot@${server} <<EOF
+				sudo systemctl stop simple-camel-logger.service
+				exit
+		       		EOF"""
 		sh 'scp target/*.jar springboot@${server}:/var/springboot/simple-camel-logger-1.0.jar'
 		sh """ssh -tt springboot@${server} << EOF 
-                                sudo systemctl restart simple-camel-logger.service
+                                sudo systemctl start simple-camel-logger.service
                                 sudo systemctl status simple-camel-logger.service
                                 exit
                                 EOF"""
